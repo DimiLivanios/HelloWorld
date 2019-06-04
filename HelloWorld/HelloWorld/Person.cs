25d998a9d12b6aeeffb86a60c51d50e93941c9c0
Dimitris Livanios <dlivanios353@gmail.com>
2019-06-04 14:30:45 +0300

    Person updated

--- HelloWorld/HelloWorld/Person.cs
+++ HelloWorld/HelloWorld/Person.cs
@@ -9,5 +9,8 @@ namespace HelloWorld
    public class Person
     {
         public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string LastName { get; set; }
     }
 }
